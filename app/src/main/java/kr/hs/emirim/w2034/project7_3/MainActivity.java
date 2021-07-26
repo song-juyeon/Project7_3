a777355d27a951a385adefe0622688edfe7f903b
SongJuyeon <80025101+song-juyeon@users.noreply.github.com>
2021-07-26 10:10:47 +0900

    Edit MainActivity.java(Dialog) Commit

--- app/src/main/java/kr/hs/emirim/w2034/project7_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2034/project7_3/MainActivity.java
@@ -1,14 +1,43 @@
 package kr.hs.emirim.w2034.project7_3;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textName, textEmail;
+    View dialogV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textName = findViewById(R.id.user_name);
+        textEmail = findViewById(R.id.user_email);
+        Button btn_input = findViewById(R.id.btn_input);
+
+        btn_input.setOnClickListener(btnListener);
+
+
     }
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dialogV = View.inflate(MainActivity.this, R.layout.dialog1, null);
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle(R.string.user_input)
+                    .setIcon(R.drawable.user)
+                    .setView(dialogV)
+                    .setPositiveButton("확인", null)
+                    .setNegativeButton("취소", null)
+                    .show();
+
+
+        }
+    };
 }
